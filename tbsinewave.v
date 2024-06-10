c053801df13549a28a4ac5a6b23cfc95ac1c6477
Lena-2023 <152642024+Lena-2023@users.noreply.github.com>
2024-06-10 21:05:58 +0300

    Add files via upload

--- tbsinewave.v
+++ tbsinewave.v
@@ -12,8 +12,11 @@ wire  [15:0]sin_val;  //signed
 
 integer freq = 0;
 real my_time = 0;
+real sin_real = 0;
 
-sinewave sin1(clk, reset, cnt, cnt_edge, sin_val );
+sinewave sin1(clk, reset, cnt, cnt_edge, sin_val);
+
+//function real sin;
 
 //assume basic clock is 10Mhz
 //reg clk;
@@ -39,10 +42,10 @@ begin
   
   freq=500;
   #10000;
-  freq=1000;
+ /* freq=1000;
   #10000;
   freq=1500;
-  #10000;
+  #10000; */
 
 
 
